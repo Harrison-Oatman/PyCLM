04a21f9cc2798bf7208c79cac046d319c342f7e8
Harrison-Oatman <ho6729@princeton.edu>
2025-01-23 18:02:22 -0500

    base message

--- src/messages.py
+++ src/messages.py
@@ -2,7 +2,7 @@
 Contains classes for passing messages between processes.
 
 These messages are allowed to contain small bits of information, but
-should not pass data that should be represented by a numpy array (see datatypes.py)
+should not pass numpy arrays (see datatypes.py)
 """
 
 from abc import ABC
