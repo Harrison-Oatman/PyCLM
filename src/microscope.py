7558509d86e63d7448cd053544769c7e82d3455d
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-01-24 02:25:33 -0500

    Update microscope.py

--- src/microscope.py
+++ src/microscope.py
@@ -1,12 +1,42 @@
 from pycromanager import Core
-from multiprocessing import Queue
+from queues import AllQueues
+from time import time
+
 
 class MicroscopeProcess:
 
-    def __init__(self, core: Core, inbox: Queue, outbox: Queue, slm_queue: Queue):
+    def __init__(self, core: Core, aq: AllQueues):
         self.core = core
-        self.inbox = inbox  # receives messages/events from the controller
-        self.outbox = outbox  # send acquisition data to outbox process
-        self.slm_queue = slm_queue  # receives SLM updates
+        self.inbox = aq.manager_to_microscope  # receives messages/events from the controller
+        self.outbox = aq.acquisition_outbox  # send acquisition data to outbox process
+        self.slm_queue = aq.slm_to_microscope  # receives SLM updates
 
-    
+    def process(self, event_await_s=5, slm_await_s=5):
+
+        event_await_start = time()
+
+        while True:
+
+            if self.inbox.empty():
+
+                # check for timeout
+                if (event_await_s != 0) & (time() - event_await_start > event_await_s):
+                    raise TimeoutError(f"No events in queue for {time() - event_await_start: .3f}s")
+
+                continue
+
+            msg = self.inbox.get()
+
+            if msg.message == "acquisition_event":
+                self.handle_acquisition_event(msg, slm_await_s)
+
+            elif msg.message == "close":
+                return 0
+
+            else:
+                raise NameError(f"Unknown message type: {msg.message}")
+
+            event_await_start = time()
+
+    def handle_acquisition_event(self, aq_event, slm_await_s):
+        pass
