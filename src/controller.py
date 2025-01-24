ae9100cfbde9b334220b63f2212932f65582dac9
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-01-24 02:25:38 -0500

    Update controller.py

--- src/controller.py
+++ src/controller.py
@@ -7,6 +7,48 @@ It is responsible for
 - scheduling microscope events
 """
 
-from pycromanager import Core
+from queues import AllQueues
 
-Core().wait_for_devices()
+
+class MicroscopeOutbox:
+
+    # grabs data from microscope, writes data to disk
+
+    def __init__(self, aq: AllQueues):
+        self.inbox = aq.manager_to_outbox
+        self.outbox = aq.acquisition_outbox
+        self.seg_queue = aq.segmentation_queue
+
+    def process(self):
+        pass
+
+
+class SLMBuffer:
+
+    def __init__(self, aq: AllQueues):
+        self.inbox = aq.manager_to_slm_buffer
+        self.slm_queue = aq.slm_to_microscope
+        self.pattern_queue = aq.pattern_to_slm
+
+    def process(self):
+        pass
+
+
+class Manager:
+
+    def __init__(self, aq: AllQueues):
+        self.msgout = {
+            "microscope": aq.manager_to_microscope,
+            "outbox": aq.manager_to_outbox,
+            "slm_buffer": aq.manager_to_slm_buffer,
+            "seg": aq.manager_to_seg,
+            "pattern": aq.manager_to_pattern,
+        }
+
+        self.msgin = {
+            "microscope": aq.microscope_to_manager,
+            "outbox": aq.outbox_to_manager,
+            "slm_buffer": aq.slm_buffer_to_manager,
+            "seg": aq.seg_to_manager,
+            "pattern": aq.pattern_to_manager,
+        }
