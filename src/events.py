be890a1b754a14cc5598d5171dc47dab62c41f08
Harrison-Oatman <ho6729@princeton.edu>
2025-01-29 17:45:07 -0500

    Update events.py

--- src/events.py
+++ src/events.py
@@ -1,5 +1,41 @@
-
+import numpy as np
 
 class AcquisitionEvent:
 
     def __init__(self):
+
+
+class Position:
+
+    def __init__(self, x=None, y=None, z=None, pfs=None, label=None):
+
+        self.label=label
+        self.x = x
+        self.y = y
+        self.z = z
+        self.pfs = pfs
+
+
+    def get_xy(self):
+        if not ((self.x is None) or (self.y is None)):
+            return [self.x, self.y]
+
+        return None
+
+    def get_z(self):
+        return self.z
+
+    def get_pfs(self):
+        return self.get_pfs
+
+
+
+# class PositionGrid:
+#     """
+#     Contains a single grid of xy(z) positions
+#     """
+#
+#     def __init__(self, label):
+#         self.label = label
+#
+#     def add_positions(self, positions):
