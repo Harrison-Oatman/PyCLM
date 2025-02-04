294c17a55cb1bb5729e8fe0840bf002e4a6e2149
Harrison-Oatman <ho6729@princeton.edu>
2025-02-04 17:38:23 -0500

    Update datatypes.py

--- src/datatypes.py
+++ src/datatypes.py
@@ -5,3 +5,9 @@ Contains classes for passing structured data between processes
 
 import numpy as np
 
+
+class EventSLMPattern:
+
+    def __init__(self, event_id, pattern):
+        self.event_id = event_id
+        self.pattern = pattern
