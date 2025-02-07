5f2b47a0edd0450571b801f76cd5044572cc8637
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-02-07 14:52:45 -0500

    pymmcoreplus

--- src/microscope.py
+++ src/microscope.py
@@ -1,6 +1,6 @@
-from pycromanager import Core
+from pymmcore_plus import CMMCorePlus
 from queues import AllQueues
-from time import time
+from time import time, sleep
 import numpy as np
 from .events import AcquisitionEvent, UpdatePatternEvent, Position
 from .datatypes import EventSLMPattern
@@ -9,7 +9,7 @@ import logging
 
 class MicroscopeProcess:
 
-    def __init__(self, core: Core, aq: AllQueues):
+    def __init__(self, core: CMMCorePlus, aq: AllQueues):
         self.core = core
         self.inbox = aq.manager_to_microscope  # receives messages/events from manager
         self.manager = aq.microscope_to_manager  # send messages to manager
@@ -23,11 +23,11 @@ class MicroscopeProcess:
 
     def declare_slm(self):
         core = self.core
-        dev = core.get_slm_device()
+        dev = core.getSLMDevice()
 
         self.slm_device = dev
-        self.slm_h = core.get_slm_height(dev)
-        self.slm_w = core.get_slm_width(dev)
+        self.slm_h = core.getSLMHeight(dev)
+        self.slm_w = core.getSLMWidth(dev)
 
         self.slm_initialized = True
 
@@ -69,7 +69,7 @@ class MicroscopeProcess:
             return 0
 
         for group, config in config_groups:
-            self.core.set_config(group, config)
+            self.core.setConfig(group, config)
 
         return 0
 
@@ -87,7 +87,7 @@ class MicroscopeProcess:
                 "bool": bool,
             }[t]
 
-            self.core.set_property(label, name, t_func(value))
+            self.core.setProperty(label, name, t_func(value))
 
     def move_to_position(self, position: Position):
 
@@ -96,21 +96,21 @@ class MicroscopeProcess:
         xy = position.get_xy()
         if xy is not None:
             logging.info(f"moving to xy {xy}")
-            core.set_xy_position(xy[0], xy[1])
+            core.setXYPosition(xy[0], xy[1])
         else:
             logging.debug(f"move_to_position called with no xy position: {position}")
 
         z = position.get_z()
         if z is not None:
             logging.info(f"moving to z position {z}")
-            core.set_position(z)
+            core.setPosition(z)
         else:
             logging.debug(f"move_to_position called with no z position: {position}")
 
         pfs = position.get_pfs()
         if pfs is not None:
             logging.info(f"setting pfs offset {pfs}")
-            core.set_auto_focus_offset(pfs)
+            core.setAutoFocusOffset(pfs)
         else:
             logging.debug(f"move_to_position called with no pfs offset: {position}")
 
@@ -130,7 +130,7 @@ class MicroscopeProcess:
         assert pattern_data is EventSLMPattern, f"received pattern data of unknown type: {type(pattern_data)}"
         assert pattern_data.event_id == event_id, f"event mismatch"
 
-        self.core.set_slm_image(self.slm_device, pattern_data)
+        self.core.setSLMImage(self.slm_device, pattern_data)
         logging.info(f"experiment {up_event.experiment_name}: set slm image")
 
         return 0
@@ -141,16 +141,35 @@ class MicroscopeProcess:
 
         self.handle_device_update(aq_event.devices)
         self.handle_config_update(aq_event.config_groups)
+        self.core.setExposure(aq_event.exposure_time_ms)
 
-        # todo: move to position
+        self.move_to_position(aq_event.position)
+
+        target_time = aq_event.scheduled_time
+        t_delta = target_time - time() - 0.1
+
+        if t_delta > 0:
+            logging.info(f"waiting {t_delta: .3f}s until next acquisition")
+            sleep(t_delta)
+
+        logging.debug("wait for system")
+        wait_time = time()
+        self.core.waitForSystem()
+        logging.debug(f"took {wait_time - time(): .3f}s")
+
+        logging.info("acquiring image")
+        image = self.snap()
+        aq_event.completed_time = time()
 
     def snap(self):
         core = self.core
 
-        core.snap_image()
+        core.snapImage()
 
-        tagged_image = core.get_tagged_image()
+        tagged_image = core.getTaggedImage()
         pixels = np.reshape(tagged_image.pix,
                             newshape=[tagged_image.tags['Height'], tagged_image.tags['Width']])
 
-        return pixels
+        tags = tagged_image.tags
+
+        return pixels, tags
