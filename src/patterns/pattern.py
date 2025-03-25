34446b4d9e6664f27d00fa64ce98634c793e0d39
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-03-25 11:37:51 -0400

    Update pattern.py

--- src/patterns/pattern.py
+++ src/patterns/pattern.py
@@ -1,9 +1,20 @@
 from ..queues import AllQueues
 
 
+class PatternModel:
+
+    def __init__(self, experiment_name):
+        self.experiment = experiment_name
+
+    def generate(self):
+        pass
+
+
 class PatternProcess:
 
     def __init__(self, aq: AllQueues):
         self.inbox = aq.manager_to_pattern
         self.manager = aq.pattern_to_manager
 
+    def process(self):
+        pass
