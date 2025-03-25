894956bedda966e4673d744208ea9b72864ee171
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-03-25 15:59:01 -0400

    AcquisitionData type

--- src/datatypes.py
+++ src/datatypes.py
@@ -4,6 +4,7 @@ Contains classes for passing structured data between processes
 
 
 import numpy as np
+from .events import AcquisitionEvent
 
 
 class EventSLMPattern:
@@ -11,3 +12,11 @@ class EventSLMPattern:
     def __init__(self, event_id, pattern):
         self.event_id = event_id
         self.pattern = pattern
+
+
+class AcquisitionData:
+
+    def __init__(self, event: AcquisitionEvent, data: np.ndarray):
+        self.event = event
+        self.event_id = event.id
+        self.data = data
