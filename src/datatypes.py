40cf6f0099c577a01c8a5ce13d4b21f442bdf198
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-04-16 14:30:58 -0400

    Update datatypes.py

--- src/datatypes.py
+++ src/datatypes.py
@@ -2,16 +2,18 @@
 Contains classes for passing structured data between processes
 """
 
-
+from uuid import uuid4
 import numpy as np
 from .events import AcquisitionEvent
 
 
 class EventSLMPattern:
 
-    def __init__(self, event_id, pattern):
+    def __init__(self, event_id, pattern, pattern_unique_id=0):
         self.event_id = event_id
         self.pattern = pattern
+
+        self.pattern_unique_id = pattern_unique_id
 
 
 class AcquisitionData:
@@ -20,3 +22,13 @@ class AcquisitionData:
         self.event = event
         self.event_id = event.id
         self.data = data
+
+
+class CameraPattern:
+
+    def __init__(self, event: AcquisitionEvent, data: np.ndarray):
+        self.event = event
+        self.experiment = event.experiment_name
+        self.data = data
+
+        self.pattern_id = uuid4()
