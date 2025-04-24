73c19e96861aa3bf1b0056aa77c0bfa7be6e8b75
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-04-24 19:49:24 -0400

    Update segmentation.py

--- src/segmentation/segmentation.py
+++ src/segmentation/segmentation.py
@@ -15,8 +15,8 @@ class SegmentationProcess:
     def __init__(self, aq: AllQueues):
         self.inbox = aq.manager_to_seg
         self.manager = aq.seg_to_manager
-        self.raw_in = aq.segmentation_queue
-        self.seg_out = aq.segmented_pattern
+        self.raw_in = aq.outbox_to_seg
+        self.seg_out = aq.seg_to_pattern
 
     def process(self):
         pass
