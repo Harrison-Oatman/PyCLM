a65382259db8984ec21fa88b04a0e76d0519bc84
Harrison-Oatman <ho6729@princeton.edu>
2025-06-17 15:02:24 -0400

    refactor pattern.py
    pulls pattern process out from pattern.py. pattern.py now contains base pattern models

---
+++ src/patterns/pattern_process.py
@@ -0,0 +1,160 @@
+import logging
+
+from src import AllQueues, CameraProperties
+from src.datatypes import CameraPattern, AcquisitionData, SegmentationData
+from src.experiments import Experiment
+from src.messages import Message
+from src.patterns import PatternReview, PatternModel, PatternModelReturnsSLM, DataDock, AcquiredImageRequest
+from src.patterns.bar_patterns import BouncingBarPattern, BarPatternBase
+from src.patterns.static_patterns import CirclePattern
+
+
+class PatternProcess:
+
+    known_models = {
+        "circle": CirclePattern,
+        "bar": BarPatternBase,
+        "pattern_review": PatternReview,
+        "bar_bounce": BouncingBarPattern,
+
+    }
+
+    def __init__(self, aq: AllQueues):
+        self.inbox = aq.manager_to_pattern
+        self.manager = aq.pattern_to_manager
+        self.slm = aq.pattern_to_slm
+
+        self.from_seg = aq.seg_to_pattern
+        self.from_raw = aq.outbox_to_pattern
+
+        self.camera_properties = None
+        self.initialized = False
+
+        self.models = {}
+        self.docks = {}
+
+    def initialize(self, camera_properties: CameraProperties):
+        self.camera_properties = camera_properties
+
+        self.initialized = True
+
+    def request_model(self, experiment: Experiment) -> list[AcquiredImageRequest]:
+
+        method_name = experiment.pattern.method_name
+
+        model_class: type = self.known_models.get(method_name)
+
+        assert model_class is not None, f"method {method_name} is not a registered model"
+        assert issubclass(model_class, PatternModel), f"{method_name} is not a PatternModel"
+
+        experiment_name = experiment.experiment_name
+        method_kwargs = experiment.pattern.kwargs
+
+        model = model_class(experiment_name, self.camera_properties, **method_kwargs)
+
+        self.models[experiment_name] = model
+
+        return model.initialize(experiment)
+
+    def register_model(self, model: type):
+
+        assert issubclass(model, PatternModel), "model must be a subclass of PatternModel"
+
+        model_name = model.name
+
+        if model_name in self.known_models:
+            logging.warning(f"overwriting known model {model_name}")
+
+        self.known_models[model_name] = model
+
+    def run_model(self, experiment_name):
+
+        data_dock = self.docks.pop(experiment_name)
+
+        model = self.models.get(experiment_name, None)
+
+        assert isinstance(model, PatternModel), f"self.models[{'experiment_name'}] is not a PatternModel"
+
+        if isinstance(model, PatternModelReturnsSLM):
+            slm_pattern = model.generate(data_dock)
+
+            self.slm.put(CameraPattern(experiment_name, slm_pattern, slm_coords=True))
+
+        else:
+
+            pattern = model.generate(data_dock)
+
+            self.slm.put(CameraPattern(experiment_name, pattern, slm_coords=False, binning=model.binning))
+
+    def check(self, experiment_name):
+
+        dock: DataDock = self.docks.get(experiment_name)
+
+        if dock.check_complete():
+            self.run_model(experiment_name)
+
+    def handle_message(self, message: Message):
+
+        match message.message:
+
+            case "request_pattern":
+
+                assert isinstance(message, RequestPattern)
+
+                req = message.requirements
+                name = message.experiment_name
+                t = message.t
+
+                dock = DataDock(t, req)
+
+                self.docks[name] = dock
+
+                self.check(name)
+
+            case _:
+                raise NotImplementedError
+
+    def process(self):
+        while True:
+            if not self.inbox.empty():
+                msg = self.inbox.get()
+
+                self.handle_message(msg)
+
+            if not self.from_raw.empty():
+                data = self.from_raw.get()
+
+                assert isinstance(data, AcquisitionData)
+                name = data.event.experiment_name
+
+                self.docks[name].add_raw(data)
+
+                self.check(name)
+
+            if not self.from_seg.empty():
+                data = self.from_seg.get()
+
+                assert isinstance(data, SegmentationData)
+                name = data.event.experiment_name
+
+                self.docks[name].add_seg(data)
+
+                self.check(name)
+
+
+class RequestPattern(Message):
+    """
+    Used by manager to inform pattern process of upcoming pattern generation
+    Pattern process will use this information to collect and store incoming
+    raw and segmented data
+    """
+
+    message = "request_pattern"
+
+    def __init__(self, t, experiment_name: str, requirements: list[AcquiredImageRequest]):
+
+        self.t = t
+        self.experiment_name = experiment_name
+        self.requirements = requirements
+
+
---
+++ src/patterns/static_patterns.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from src.patterns import PatternModel, DataDock
+
+
+class CirclePattern(PatternModel):
+    """
+    Projects a circle of radius rad in the center of the camera
+    """
+
+    name = "circle"
+
+    def __init__(self, experiment_name, camera_properties, rad=1, **kwargs):
+        super().__init__(experiment_name, camera_properties)
+
+        self.rad = rad
+
+    def initialize(self, experiment):
+
+        super().initialize(experiment)
+
+        return []
+
+    def generate(self, data_dock: DataDock):
+
+        h, w = self.pattern_shape
+
+        center_x = self.pixel_size_um * w / 2.
+        center_y = self.pixel_size_um * h / 2.
+
+        xx, yy = self.get_meshgrid()
+
+        print(h, w)
+
+        # pattern = np.ones((int(h), int(w)), dtype=np.float16)
+
+        return (((xx - center_x)**2 + (yy - center_y)**2) < (self.rad**2)).astype(np.float16)
+
+
+class FullOnPattern(PatternModel):
+    """
+    Turns on the entire ROI
+    """
+
+    name = "full_on"
+
+    def __init__(self, experiment_name, camera_properties, **kwargs):
+        super().__init__(experiment_name, camera_properties)
+
+    def initialize(self, experiment):
+        super().initialize(experiment)
+        return []
+
+    def generate(self, data_dock: DataDock):
+
+        h, w = self.pattern_shape
+        pattern = np.ones((int(h), int(w)), dtype=np.float16)
+
+        return pattern
