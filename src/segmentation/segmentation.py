8f79a50dc6e297e736493da009d827bb0fdac5ce
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-06-18 23:56:05 -0400

    Update segmentation.py

--- src/segmentation/segmentation.py
+++ src/segmentation/segmentation.py
@@ -1,13 +1,21 @@
+import logging
+import numpy as np
+
 from ..queues import AllQueues
+from ..experiments import Experiment
+from ..datatypes import AcquisitionData, SegmentationData
+from ..messages import Message
 
 
 class SegmentationModel:
+    name = "base_model"
 
-    def __init__(self):
+    def __init__(self, experiment_name, **kwargs):
         pass
 
-    def segment(self):
-        pass
+    def segment(self, data: np.ndarray) -> np.ndarray:
+
+        raise NotImplementedError("base_model does not implement segment")
 
 
 class SegmentationProcess:
@@ -20,37 +28,34 @@ class SegmentationProcess:
         self.manager = aq.seg_to_manager
 
         self.from_raw = aq.outbox_to_seg
-        self.to_seg = aq.seg_to_pattern
+        self.to_pattern = aq.seg_to_pattern
 
         self.initialized = False
 
         self.models = {}
-        self.docks = {}
 
     def initialize(self):
         self.initialized = True
 
-    def request_model(self, experiment: Experiment) -> list[AcquiredImageRequest]:
+    def request_model(self, experiment: Experiment):
 
         method_name = experiment.pattern.method_name
 
         model_class: type = self.known_models.get(method_name)
 
         assert model_class is not None, f"method {method_name} is not a registered model"
-        assert issubclass(model_class, PatternModel), f"{method_name} is not a PatternModel"
+        assert issubclass(model_class, SegmentationModel), f"{method_name} is not a PatternModel"
 
         experiment_name = experiment.experiment_name
         method_kwargs = experiment.pattern.kwargs
 
-        model = model_class(experiment_name, self.camera_properties, **method_kwargs)
+        model = model_class(experiment_name, **method_kwargs)
 
         self.models[experiment_name] = model
 
-        return model.initialize(experiment)
-
     def register_model(self, model: type):
 
-        assert issubclass(model, PatternModel), "model must be a subclass of PatternModel"
+        assert issubclass(model, SegmentationModel), "model must be a subclass of PatternModel"
 
         model_name = model.name
 
@@ -59,70 +64,37 @@ class SegmentationProcess:
 
         self.known_models[model_name] = model
 
-    def run_model(self, experiment_name):
-
-        data_dock = self.docks.pop(experiment_name)
+    def run_model(self, experiment_name, aq_data: AcquisitionData) -> SegmentationData:
 
         model = self.models.get(experiment_name, None)
 
-        assert isinstance(model, PatternModel), f"self.models[{'experiment_name'}] is not a PatternModel"
+        assert isinstance(model, SegmentationModel), f"self.models[{'experiment_name'}] is not a PatternModel"
 
-        if isinstance(model, PatternModelReturnsSLM):
-            slm_pattern = model.generate(data_dock)
+        data_to_seg = aq_data.data
+        segmented = model.segment(data_to_seg)
 
-            self.slm.put(CameraPattern(experiment_name, slm_pattern, slm_coords=True))
+        event = aq_data.event
+        seg_data = SegmentationData(event, segmented)
 
-        else:
+        return seg_data
 
-            pattern = model.generate(data_dock)
+    def handle_segment_data(self, aq_data: AcquisitionData):
 
-            self.slm.put(CameraPattern(experiment_name, pattern, slm_coords=False, binning=model.binning))
+        name = aq_data.event.experiment_name
 
-    def check(self, experiment_name):
-
-        dock: DataDock = self.docks.get(experiment_name)
-
-        # print(dock.get_awaiting())
-
-        if dock.check_complete():
-            self.run_model(experiment_name)
+        seg_data = self.run_model(name, aq_data)
 
     def handle_message(self, message: Message):
 
-        # print(message.message)
-
         match message.message:
-
-            case "request_pattern":
-
-                assert isinstance(message, RequestPattern)
-
-                req = message.requirements
-                name = message.experiment_name
-                t = message.t
-
-                dock = DataDock(t, req)
-
-                self.docks[name] = dock
-
-                self.check(name)
-
-            # case "request_method":
-            #
-            #     assert isinstance(message, RequestMethod)
-            #     requirements = self.request_model(message.experiment)
-            #
-            #     returned_message = PatternRequirements(requirements)
-            #
-            #     self.manager.put(returned_message)
-            #
-            #     return None
 
             case _:
                 raise NotImplementedError
 
     def process(self):
+
         while True:
+
             if not self.inbox.empty():
                 msg = self.inbox.get()
 
@@ -132,18 +104,4 @@ class SegmentationProcess:
                 data = self.from_raw.get()
 
                 assert isinstance(data, AcquisitionData)
-                name = data.event.experiment_name
-
-                self.docks[name].add_raw(data)
-
-                self.check(name)
-
-            if not self.from_seg.empty():
-                data = self.from_seg.get()
-
-                assert isinstance(data, SegmentationData)
-                name = data.event.experiment_name
-
-                self.docks[name].add_seg(data)
-
-                self.check(name)
+                self.handle_segment_data(data)
