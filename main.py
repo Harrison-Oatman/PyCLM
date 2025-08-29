d81e827afac0fe037a70f42da585ba53c2f1373a
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-08-29 14:08:45 -0400

    Restructure main
    Pulled controller out of main.py
    main.py has function run_pyclm()
    main() just calls argparse and uses run_pyclm
    
    This allows for command-line or jupyter notebook usage

--- main.py
+++ main.py
@@ -1,110 +1,21 @@
 from argparse import ArgumentParser
 import logging
-import traceback
+from toml import load
+from typing import Optional
 
 import numpy as np
 
-from pyclm.src import *
-from pymmcore_plus import CMMCorePlus
 # from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
+
+from pyclm import Controller, SegmentationMethod, PatternMethod, schedule_from_directory
 
 logger = logging.getLogger(__name__)
 
 
-class Controller:
-
-    def __init__(self, config="MMConfig_demo.cfg"):
-        self.core = CMMCorePlus()
-        self.core.loadSystemConfiguration(config)
-        self.all_queues = AllQueues()
-
-        self.microscope = MicroscopeProcess(core=self.core, aq=self.all_queues)
-        self.manager = Manager(aq=self.all_queues)
-        self.outbox = MicroscopeOutbox(aq=self.all_queues, save_type="hdf5")
-        self.slm_buffer = SLMBuffer(aq=self.all_queues)
-        self.segmentation = SegmentationProcess(aq=self.all_queues)
-        self.pattern = PatternProcess(aq=self.all_queues)
-
-        self.processes = [
-            self.microscope,
-            self.manager,
-            self.outbox,
-            self.slm_buffer,
-            self.segmentation,
-            self.pattern
-        ]
-
-        self.camera_properties = None
-
-    def set_binning(self, binning: int):
-        core = self.core
-        camera = self.core.getCameraDevice()
-
-        try:
-            allowed = core.getAllowedPropertyValues(camera, "Binning")
-        except:
-            return None
-
-        binning_str = f"{binning}x{binning}"
-
-        if binning_str in allowed:
-            core.setProperty(camera, "Binning", binning_str)
-
-        else:
-            logger.warning(f"attempted set binning {binning_str}, allowed binnings {allowed}")
-
-    def initialize(self, schedule: ExperimentSchedule, slm_shape: tuple[int, int], affine_transform: np.ndarray,
-                   out_path: Path):
-
-        self.set_binning(1)
-
-        camera_roi = ROI(*self.core.getROI())
-        camera_resolution = self.core.getPixelSizeUm()
-
-        self.camera_properties = CameraProperties(camera_roi, camera_resolution)
-
-        logger.info(f"camera properties: {self.camera_properties}")
-
-        self.pattern.initialize(self.camera_properties)
-
-        pattern_requirements = {}
-        for name, experiment in schedule.experiments.items():
-            pattern_requirements[name] = self.pattern.request_model(experiment)
-
-            if any([req.needs_seg for req in pattern_requirements[name]]):
-                self.segmentation.request_model(experiment)
-
-        self.manager.initialize(schedule, pattern_requirements)
-        self.slm_buffer.initialize(slm_shape, affine_transform, schedule.experiment_names)
-        self.microscope.declare_slm()
-        self.outbox.base_path = out_path
-
-    def run(self):
-        with ThreadPoolExecutor() as executor:
-            futures = [executor.submit(process.process) for process in self.processes]
-            for future in as_completed(futures):
-                try:
-                    future.result()
-                except Exception as e:
-                    logging.error(f"Exception occurred: {e}")
-                    logging.error(traceback.format_exc())
-                # future.result()
-
-
-def get_slm_shape(core: CMMCorePlus):
-
-    dev = core.getSLMDevice()
-    return core.getSLMHeight(dev), core.getSLMWidth(dev)
-
-
-def main():
-    args = process_args()
-    base_path = Path(str(r"E:\Harrison\RTx3 imaging\2025-08-21 tag-rfp 1\global cycle"))
-
+def set_logging(experiment_directory: Path):
     console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(base_path / 'log.log')
+    file_handler = logging.FileHandler(experiment_directory / 'log.log')
 
     # Set levels for handlers
     console_handler.setLevel(logging.WARNING)
@@ -120,73 +31,85 @@ def main():
 
     logging.basicConfig(handlers=[console_handler, file_handler])
 
-    c = Controller(r"C:\Program Files\Micro-Manager-2.0\Ti2MightexCrestSolaSpectra.cfg")
+
+def run_pyclm(experiment_directory, config_path=None,
+              segmentation_methods: Optional[dict[str, type[SegmentationMethod]]]= None,
+              pattern_methods: Optional[dict[str, type[PatternMethod]]]= None):
+    """
+    Run a pyclm experiment from a given directory and configuration file.
+    :param experiment_directory: directory containing experiment files, including schedule.toml. [experiment].toml files,
+                               and the position list
+    :param config_path: path to pyclm_config.toml file. If None, will look for pyclm_config.toml in the experiment_directory
+    :param segmentation_methods: optional dictionary of segmentation method classes to register with the SegmentationProcess
+                                    key is the method name (used by [experiment].toml), value is the class
+    :param pattern_methods: optional dictionary of pattern method classes to register with the PatternProcess
+                                    key is the method name (used by [experiment].toml), value is the class
+    :return:
+    """
+
+    experiment_directory = Path(experiment_directory)
+
+    # search for config file if not provided
+    if config_path is None:
+        # look in the experiment directory for pyclm_config.toml
+        config_path = experiment_directory / "pyclm_config.toml"
+
+        # look in the current working directory for pyclm_config.toml
+        if not config_path.exists():
+            config_path = Path("pyclm_config.toml")
+
+    config_path = Path(config_path)
+
+    assert experiment_directory.exists(), f"experiment directory {experiment_directory} does not exist"
+    assert config_path.exists(), (f"config file {config_path} does not exist: pyclm_config.toml must be specified or be "
+                                  f"present in the experiment directory")
+
+    set_logging(experiment_directory)
+
+    config = load(config_path)
+    logger.info(f"loaded config from {config_path}")
+
+    base_path = experiment_directory
+
+    c = Controller(config["microscope_config"])
+
+    # register any custom methods
+    if segmentation_methods is not None:
+        for name, method in segmentation_methods.items():
+            c.register_segmentation_method(name, method)
+
+    if pattern_methods is not None:
+        for name, method in pattern_methods.items():
+            c.register_pattern_method(name, method)
 
     core = c.core
     core.describe()
 
+    print("---listing available config groups---")
     for group in core.getAvailableConfigGroups():
         cg = core.getConfigGroupObject(group, False)
         print(cg.name, list(cg.items()))
 
     schedule = schedule_from_directory(base_path)
 
-    slm_shape = get_slm_shape(core)
+    slm_shape = config["slm_shape_h"], config["slm_shape_w"]
+    at = np.array(config["affine_transform"], dtype=np.float32)
 
-    core.setFocusDevice("ZDrive")
-
-    print(core.getAutoFocusDevice())
-    print(core.getAutoFocusOffset())
-
-    core.setAutoFocusOffset(list(schedule.positions.values())[0].get_autofocus_offset())
-
-    at = np.array([[-.289, 0.006, 959.025], [-0.012, -0.579, 1540.03]], dtype=np.float32)
     c.initialize(schedule, slm_shape, at, base_path)
-    # logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
 
     c.run()
 
 
-# def main():
-#     args = process_args()
-#
-#     c = Controller()
-#
-#     core = c.core
-#     for group in core.getAvailableConfigGroups():
-#         cg = core.getConfigGroupObject(group, False)
-#         print(list(cg.items()))
-#
-#     t_interval = 5
-#     t_steps = 5
-#     t_setup = 2
-#     t_between = 1
-#     e1_toml_path = r"D:\FeedbackControl\experiments\SampleExperiment.toml"
-#     e2_toml_path = r"D:\FeedbackControl\experiments\SampleExperiment2.toml"
-#     experiments = {"a": experiment_from_toml(e1_toml_path, "a"),
-#                    "b": experiment_from_toml(e2_toml_path, "b")}
-#     positions = {
-#         "a": Position(1, 2, 0),
-#         "b": Position(3, 4, 0)
-#     }
-#
-#     schedule = ExperimentSchedule(experiments, positions, t_steps, t_interval, t_setup, t_between)
-#     slm_shape = (512, 512)
-#     at = np.array([[1, 0, 0], [0, 1, 0]], dtype=np.float32)
-#     base_path = Path(r"D:\FeedbackControl\test")
-#
-#     c.initialize(schedule, slm_shape, at, base_path)
-#
-#     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
-#
-#     c.run()
+def main():
+    args = process_args()
+    base_path = Path(args.directory)
+    run_pyclm(base_path, args.config)
 
 
 def process_args():
     parser = ArgumentParser()
-    # parser.add_argument("--config", type=str, help="path to config file")
-    # parser.add_argument("--log", type=str, help="path to log file")
-    parser.add_argument("--debug", action="store_true", help="enable debug logging")
+    parser.add_argument("directory", help="directory containing experiment files")
+    parser.add_argument("--config", type=str, help="path to pyclm_config.toml file", default=None)
 
     return parser.parse_args()
 
---
+++ pyclm/src/controller.py
@@ -0,0 +1,98 @@
+import logging
+import traceback
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from pathlib import Path
+
+from pymmcore_plus import CMMCorePlus
+
+import numpy as np
+
+from main import logger
+from pyclm.src import AllQueues, MicroscopeProcess, Manager, MicroscopeOutbox, SLMBuffer, SegmentationProcess, \
+    PatternProcess, ExperimentSchedule, ROI, CameraProperties
+
+
+class Controller:
+
+    def __init__(self, config="MMConfig_demo.cfg"):
+        self.core = CMMCorePlus()
+        self.core.loadSystemConfiguration(config)
+        self.all_queues = AllQueues()
+
+        self.microscope = MicroscopeProcess(core=self.core, aq=self.all_queues)
+        self.manager = Manager(aq=self.all_queues)
+        self.outbox = MicroscopeOutbox(aq=self.all_queues, save_type="hdf5")
+        self.slm_buffer = SLMBuffer(aq=self.all_queues)
+        self.segmentation = SegmentationProcess(aq=self.all_queues)
+        self.pattern = PatternProcess(aq=self.all_queues)
+
+        self.processes = [
+            self.microscope,
+            self.manager,
+            self.outbox,
+            self.slm_buffer,
+            self.segmentation,
+            self.pattern
+        ]
+
+        self.camera_properties = None
+
+    def set_binning(self, binning: int):
+        core = self.core
+        camera = self.core.getCameraDevice()
+
+        try:
+            allowed = core.getAllowedPropertyValues(camera, "Binning")
+        except:
+            return None
+
+        binning_str = f"{binning}x{binning}"
+
+        if binning_str in allowed:
+            core.setProperty(camera, "Binning", binning_str)
+
+        else:
+            logger.warning(f"attempted set binning {binning_str}, allowed binnings {allowed}")
+
+    def register_pattern_method(self, name: str, method: type):
+        self.pattern.register_method(method, name)
+
+    def register_segmentation_method(self, name: str, method: type):
+        self.segmentation.register_method(method, name)
+
+    def initialize(self, schedule: ExperimentSchedule, slm_shape: tuple[int, int], affine_transform: np.ndarray,
+                   out_path: Path):
+
+        self.set_binning(1)
+
+        camera_roi = ROI(*self.core.getROI())
+        camera_resolution = self.core.getPixelSizeUm()
+
+        self.camera_properties = CameraProperties(camera_roi, camera_resolution)
+
+        logger.info(f"camera properties: {self.camera_properties}")
+
+        self.pattern.initialize(self.camera_properties)
+
+        pattern_requirements = {}
+        for name, experiment in schedule.experiments.items():
+            pattern_requirements[name] = self.pattern.request_method(experiment)
+
+            if any([req.needs_seg for req in pattern_requirements[name]]):
+                self.segmentation.request_method(experiment)
+
+        self.manager.initialize(schedule, pattern_requirements)
+        self.slm_buffer.initialize(slm_shape, affine_transform, schedule.experiment_names)
+        self.microscope.declare_slm()
+        self.outbox.base_path = out_path
+
+    def run(self):
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(process.process) for process in self.processes]
+            for future in as_completed(futures):
+                try:
+                    future.result()
+                except Exception as e:
+                    logging.error(f"Exception occurred: {e}")
+                    logging.error(traceback.format_exc())
+                # future.result()
