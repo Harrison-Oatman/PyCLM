8fd89aa3acfa57bef048483a53c10333225d1415
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-09-22 23:17:39 -0400

    Update controller.py

--- src/pyclm/controller.py
+++ src/pyclm/controller.py
@@ -7,7 +7,7 @@ from pymmcore_plus import CMMCorePlus
 
 import numpy as np
 
-from src.pyclm import AllQueues, MicroscopeProcess, Manager, MicroscopeOutbox, SLMBuffer, SegmentationProcess, \
+from .core import AllQueues, MicroscopeProcess, Manager, MicroscopeOutbox, SLMBuffer, SegmentationProcess, \
     PatternProcess, ExperimentSchedule, ROI, CameraProperties
 
 logger = logging.getLogger(__name__)
